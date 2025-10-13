b071329d1859ec2fd169940b312a6aa59bf6c400
LandBasedFighter <moguinyard@gmail.com>
2025-10-13 15:12:38 -0400

    feat: new 'novelty' score based on chroma, switched chroma time frequency analysis to cqt

--- backend/main.py
+++ backend/main.py
@@ -31,45 +31,60 @@ def extract_features(path=None):
     rms_diff = np.diff(librosa.feature.rms(y=y)[0]) 
     zcr_diff = np.diff(librosa.feature.zero_crossing_rate(y)[0])
     onset_diff = np.diff(librosa.onset.onset_strength(y=y, sr=sr))
-    chroma_diff = np.diff(librosa.feature.chroma_stft(y=y, sr=sr)[0]) #lol c.diff
+    chroma_diff = np.diff(librosa.feature.chroma_cqt(y=y, sr=sr)[0]) #lol c.diff
     #times for plotting
     times = librosa.frames_to_time(np.arange(len(rms_diff)), sr=sr)
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
     beat_times = librosa.frames_to_time(beats, sr=sr)
 
-    return times, tempo, beat_times, rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff
+    return times, tempo, beat_times, rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff, y, sr
 
 
 
-def detect_cues (rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff, times, beat_times, tempo):
-    #TODO: implement feature matrix
+def detect_cues (rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff, times, beat_times, tempo,y, sr):
 
     scaler = sk.preprocessing.MinMaxScaler()
     rms_diff_z = scaler.fit_transform(rms_diff.reshape(-1, 1)).flatten()
     cent_diff_z = scaler.fit_transform(cent_diff.reshape(-1, 1)).flatten() 
     zcr_diff_z = scaler.fit_transform(zcr_diff.reshape(-1, 1)).flatten()
     onset_diff_z = scaler.fit_transform(onset_diff.reshape(-1, 1)).flatten()
-    chroma_diff_z = scaler.fit_transform(chroma_diff.reshape(-1, 1)).flatten()
-
+    chroma_diff_z = scaler.fit_transform(chroma_diff.reshape(-1, 1)).flatten() # chroma will also be used to calculate novelty 
     #smooth all features with a gaussian filter
     rms_diff_z = scipy.ndimage.gaussian_filter1d(rms_diff_z, sigma=2)
     cent_diff_z = scipy.ndimage.gaussian_filter1d(cent_diff_z, sigma=2)
     zcr_diff_z = scipy.ndimage.gaussian_filter1d(zcr_diff_z, sigma=2)
     onset_diff_z = scipy.ndimage.gaussian_filter1d(onset_diff_z, sigma=2)
     chroma_diff_z = scipy.ndimage.gaussian_filter1d(chroma_diff_z, sigma=2)
-    
-    #TODO: try using kmeans clustering to identify sections of high activity instead of peak finding, or in conjunction with peak finding
-    #weighted fusion of features
-    #weights determined by trial and error
-    score = (0.4 * rms_diff_z + 
-                0.13 * cent_diff_z + 
-                0.1 * zcr_diff_z + 
-                0.25 * onset_diff_z +
-                0.15 * chroma_diff_z)
-    
-    #boost onset
-    score = score * (1 + onset_diff_z)
 
+    #calculate novelty from chroma using ssm
+
+    ssm = librosa.segment.recurrence_matrix(chroma_diff_z, mode='affinity')
+    novelty = np.sqrt(np.sum(np.diff(ssm, axis=1)**2, axis=0))
+
+
+
+    # make all features the same length, replace with extracting everything with the same hop length later
+    min_len = min(len(rms_diff_z), len(cent_diff_z), len(zcr_diff_z), 
+              len(onset_diff_z), len(chroma_diff_z), len(novelty))
+
+    rms_diff_z = rms_diff_z[:min_len]
+    cent_diff_z = cent_diff_z[:min_len]
+    zcr_diff_z = zcr_diff_z[:min_len]
+    onset_diff_z = onset_diff_z[:min_len]
+    chroma_diff_z = chroma_diff_z[:min_len]
+    novelty = novelty[:min_len]
+    times = times[:min_len]
+
+    #combine features into a single score
+    #weights determined by trial and error
+    score = (0.3 * rms_diff_z + 
+             0.1 * cent_diff_z + 
+             0.05 * zcr_diff_z + 
+             0.2 * onset_diff_z +
+            0.05 * chroma_diff_z + 
+             0.3 * novelty)
+    
+    
     #peak finding
 
     #only consider peaks above 90th percentile of score within a local window of 20 seconds, 
@@ -110,6 +125,6 @@ def detect_cues (rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff, times, 
 
 
 print("Welcome to the audio feature extractor!")
-times, tempo, beat_times, rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff = extract_features()
-cue_times = detect_cues(rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff, times, beat_times, tempo)
+times, tempo, beat_times, rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff,y,sr = extract_features()
+cue_times = detect_cues(rms_diff, cent_diff, zcr_diff, onset_diff, chroma_diff, times, beat_times, tempo,y,sr)
 print("Expected cue points", cue_times) 
